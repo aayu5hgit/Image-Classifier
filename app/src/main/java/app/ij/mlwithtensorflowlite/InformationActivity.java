5b0fc21c902d1dd81d967ffd4da49ed7128054d1
AAYUSH TALREJA <amtalreja02@gmail.com>
2022-03-12 21:57:28 +0530

    Information activity added

---
+++ app/src/main/java/app/ij/mlwithtensorflowlite/InformationActivity.java
@@ -0,0 +1,14 @@
+package app.ij.mlwithtensorflowlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+    }
+}
