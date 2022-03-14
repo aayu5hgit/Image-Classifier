5a83841f338d2a0ab06fd3c0b4e06e0ef108670a
AAYUSH TALREJA <amtalreja02@gmail.com>
2022-03-14 19:50:21 +0530

    Minor

--- app/src/main/java/app/ij/mlwithtensorflowlite/LilyActivity.java
+++ app/src/main/java/app/ij/mlwithtensorflowlite/LilyActivity.java
@@ -4,18 +4,26 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class LilyActivity extends AppCompatActivity {
-
+    Button readmorebtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lily);
+
+        readmorebtn = findViewById(R.id.readmorelily);
+        Intent browserIntent = new Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://en.wikipedia.org/wiki/Lilium"));
+        startActivity(browserIntent);
 
     }
 }
