3243cb7a0e4c76fb46fb364573b101bb894ab323
AAYUSH TALREJA <amtalreja02@gmail.com>
2022-03-25 22:31:33 +0530

    Rose Activity Added

---
+++ app/src/main/java/app/ij/mlwithtensorflowlite/RoseActivity.java
@@ -0,0 +1,36 @@
+package app.ij.mlwithtensorflowlite;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageButton;
+
+public class RoseActivity extends AppCompatActivity {
+    ImageButton backbtn;
+    Window window;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rose);
+        //STATUS BAR COLOR:
+        if (Build.VERSION.SDK_INT >= 15) {
+            window = this.getWindow();
+            window.setStatusBarColor(this.getResources().getColor(R.color.main2));
+        }
+
+        backbtn = findViewById(R.id.backbtn);
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
